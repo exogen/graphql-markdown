13591dc08b137b71b9da6921bdfd4227dd5e8b1f
Brian Beck <exogen@gmail.com>
2018-08-03 22:08:10 -0700

    Bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  extends: [
-    'standard',
-    'prettier',
-    'prettier/standard'
-  ],
+  extends: ['standard', 'prettier', 'prettier/standard'],
   env: {
     es6: true,
     node: true
   },
+  parserOptions: {
+    ecmaVersion: 6
+  },
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['warn', {
-      singleQuote: true,
-      semi: false
-    }]
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false
+      }
+    ]
   }
 }
