5491f85cf96e691531b73b6c19c6ce7d4ce4dc85
Brian Beck <exogen@users.noreply.github.com>
2018-09-27 23:28:27 -1000

    Bump dependencies, migrate to Jest, improve tests (#35)

---
+++ test/fixtures/cover-art-archive.js
@@ -0,0 +1,28 @@
+const resolveFrom = require('resolve-from')
+const {
+  default: graphBrainzSchema,
+  createSchema
+} = require('graphbrainz/lib/schema')
+const { schemaToJSON, diffSchema } = require('../../src/index')
+
+/**
+ * Generate a "diff" schema in order to test `diffSchema`.
+ */
+async function generateDiff() {
+  // Get the instance of `graphql` that `graphbrainz` sees.
+  const graphql = require(resolveFrom(
+    require.resolve('graphbrainz'),
+    'graphql'
+  ))
+
+  const extendedSchema = createSchema(graphBrainzSchema, {
+    extensions: ['graphbrainz/extensions/cover-art-archive']
+  })
+
+  const schemaJSON = await schemaToJSON(graphBrainzSchema, { graphql })
+  const extendedSchemaJSON = await schemaToJSON(extendedSchema, { graphql })
+
+  return diffSchema(schemaJSON, extendedSchemaJSON)
+}
+
+module.exports = generateDiff()
